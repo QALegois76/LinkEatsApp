72bf0bd9248e6cc438f5c4a70cb0561e2d6737f7
ayonis\antoine.legois <antoine.legois@altimet.fr>
2022-12-16 17:42:30 +0100

    EF implemented

---
+++ ConsoleAppEntityFramework/Migrations/20221216163543_AddDiscountLink.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ConsoleAppEntityFramework.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDiscountLink : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "OrderID",
+                table: "Discounts",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Discounts_OrderID",
+                table: "Discounts",
+                column: "OrderID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Discounts_Orders_OrderID",
+                table: "Discounts",
+                column: "OrderID",
+                principalTable: "Orders",
+                principalColumn: "OrderID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Discounts_Orders_OrderID",
+                table: "Discounts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Discounts_OrderID",
+                table: "Discounts");
+
+            migrationBuilder.DropColumn(
+                name: "OrderID",
+                table: "Discounts");
+        }
+    }
+}
